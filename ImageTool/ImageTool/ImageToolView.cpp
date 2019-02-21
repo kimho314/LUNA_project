1aca43c42d2b9ba73203406be21fda606a37d1ae
Hoseop Kim <kimho314@gmail.com>
2019-02-21 20:05:21 +0900

    refectoring color segmentation

--- ImageTool/ImageTool/ImageToolView.cpp
+++ ImageTool/ImageTool/ImageToolView.cpp
@@ -192,8 +192,7 @@ void CImageToolView::OnLButtonUp(UINT nFlags, CPoint point)
 		CPen myPen, *pOldPen;
 		myPen.CreatePen(PS_SOLID, 1, RGB(255, 255, 255));
 		pOldPen = dc.SelectObject(&myPen);
-
-		printf("left top : (%3d, %3d), right down : (%3d, %3d)\n", m_LeftTopX, m_LeftTopY, m_RightDownX, m_RightDownY);
+		
 
 		dc.Rectangle(m_LeftTopX, m_LeftTopY, m_RightDownX, m_RightDownY);
 		dc.SelectObject(pOldBrush);
